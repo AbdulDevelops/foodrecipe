614098070591b5c83931253b0e85d05fc6e0f8d7
Abdul Rehman <85558577+AbdulDevelops@users.noreply.github.com>
2023-07-31 00:54:59 +0200

    Selection of Recipe dynamically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'runing a test on Recipe',
     'https://www.sheknows.com/wp-content/uploads/2018/08/ti8wzfbbvdspxo8dg1ci.jpeg?w=1920'),
@@ -20,5 +22,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(){
 
   }
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
 
+  }
 }
