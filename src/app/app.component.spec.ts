90209727523a0820976195db9bb7c795740789c4
Abdul Rehman <85558577+AbdulDevelops@users.noreply.github.com>
2024-02-22 09:25:35 +0100

    testing configurations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataStorageService } from './shared/data-storage.service';
+import { RecipeService } from './recipes/recipes.service';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    imports: [HttpClientModule, RouterModule], // Add HttpClientModule to imports
+    providers: [DataStorageService, RecipeService  ],
+    declarations: [AppComponent, HeaderComponent,  ]
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should be created', () => {
+    const service: DataStorageService = TestBed.inject(DataStorageService);
+    expect(service).toBeTruthy();
   });
-
-  it(`should have as title 'foodrecipe'`, () => {
+  /* it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('foodrecipe');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
     fixture.detectChanges();
+
+    expect(app).toBeTruthy();
+  }); */
+
+   it(`should have as title 'foodrecipe'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    expect(app.title).toEqual('foodrecipe');
+  }); 
+
+/*   it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     const compiled = fixture.nativeElement as HTMLElement;
+    
+    fixture.detectChanges(); 
     expect(compiled.querySelector('.content span')?.textContent).toContain('foodrecipe app is running!');
-  });
+  }); */
 });
+
